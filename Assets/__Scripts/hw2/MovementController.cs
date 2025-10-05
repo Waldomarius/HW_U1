103244d67c0f54e21845d283374463b964f0a14c
Waldomarius <40439764+Waldomarius@users.noreply.github.com>
2025-10-05 19:31:08 +0300

    HW_2: Add movement for 2D Player.

---
+++ Assets/__Scripts/hw2/MovementController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace __Scripts.hw2
+{
+    public class MovementController : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _jumpForce = 5f;
+        private Vector2 _movement;
+
+        [Header("Ground")]
+        [SerializeField] private Transform _groundCheck; 
+        [SerializeField] private float _groundCheckRadius = 0.2f;
+        [SerializeField] private LayerMask _groundLayer;
+        
+        [Header("Player")]
+        [SerializeField] private GameObject _player;
+        
+        
+        private Rigidbody2D _rigidbody;
+        private SpriteRenderer _sprite;
+        
+        private bool _isGrounded; 
+        private bool _isFacingRight = true; 
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _sprite  = _player.GetComponent<SpriteRenderer>();
+
+        }
+
+        private void Update()
+        {
+            _movement.x = Input.GetAxis("Horizontal");
+            _rigidbody.velocity = new Vector2(_movement.x * _speed, _rigidbody.velocity.y);
+            
+            _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+            }
+            
+            if (_movement.x > 0 && !_isFacingRight || _movement.x < 0 && _isFacingRight)
+            {
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            _sprite.flipX = _isFacingRight;
+            _isFacingRight = !_isFacingRight;
+        }
+    }
+}
---
+++ Assets/__Scripts/hw2/Player.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace __Scripts.hw2
+{
+    public class Player  : MonoBehaviour
+    {
+        
+    }
+}
